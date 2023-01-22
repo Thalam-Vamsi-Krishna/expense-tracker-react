ee49f439ff940f5e3ac94f1e9b4425ca9be71091
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2023-01-22 09:57:42 +0530

    successfully implemented

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  const itemEntered = (event) => {
-    console.log(event.target.value);
+  const [enteredItem, setEnteredItem] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const itemChangeHandler = (event) => {
+    setEnteredItem(event.target.value);
+  };
+  const priceChangeHandler = (event) => {
+    setEnteredPrice(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2023-01-31" />
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2023-01-31"
+            onChange={dateChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Item</label>
-          <input type="text" onChange={itemEntered} />
+          <input type="text" onChange={itemChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={priceChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
